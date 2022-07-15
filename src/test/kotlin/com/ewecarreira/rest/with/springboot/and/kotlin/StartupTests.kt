244dbc00741c19cb58b20c15163db5687b19eaca
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-07-15 11:31:08 -0300

    first commit

---
+++ src/test/kotlin/com/ewecarreira/rest/with/springboot/and/kotlin/StartupTests.kt
@@ -0,0 +1,13 @@
+package com.ewecarreira.rest.with.springboot.and.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
